ea07cddfdceaba5f1ed93475d3422412bc520b95
Eric Tran <Eric@Erics-MBP.hsd1.ca.comcast.net>
2015-08-09 17:34:54 -0700

    Initial Commit

---
+++ EasyBudget/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasyBudget
+//
+//  Created by Eric Tran on 8/9/15.
+//  Copyright (c) 2015 Eric Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
